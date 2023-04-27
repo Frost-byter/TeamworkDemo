fb1549dcb8c54b9767c8cbbcb584c0a4d3c8a9e1
MilesMixon <52500397+MilesMixon@users.noreply.github.com>
2023-04-27 16:00:29 -0400

    Revert "Merge pull request #15 from MilesMixon/scmalloy-patch-1"
    This reverts commit da36e412496429cd2a3399c7a69f363b53a2c716, reversing
    changes made to eaebacb6c3001fdc7d8ae6a4a7189a7e25515cc7.

--- MyCounter.java
+++ MyCounter.java
@@ -1,12 +1,26 @@
-package com.skillstorm.general;
-
 public class MyCounter extends Thread {
-	
-	@Override
-	public void run() {
-		// Sarah Malloy
-		for (int i = 0; i < 58; i+=2) {
-			System.out.println(i + " ");
-		}
-	}
+
+    /*
+     * Make a branch
+     * Implement this run method
+     * 
+     * What does run do? 
+     *  It counts from 0 to any number of your choosing
+     *  It counts by any increment of your choosing
+     *  It prints the number out when it's finished
+     * 
+     * I want you to merge your method into the main branch
+     * Upload the file you have locally after you merge it
+     * add a comment with your name next to your loop in the run method
+     * 
+     * While mergeing DO NOT overrite someone else's code
+     */
+
+    @Override 
+    public void run() {
+        // kyle cook
+        for(int i = 0; i < 22; i+=3) {
+            System.out.print(i + "$");
+        }
+    }
 }
